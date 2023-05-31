82fb8f8c6a017a38c7940c79af52347238ca3773
Igor Cardoso <programacaoudemyigr@gmail.com>
2023-05-31 16:40:31 -0300

    EstruturaDeDecisão

---
+++ exercicio_decrescente.py
@@ -0,0 +1,13 @@
+var1 = int(input('Primeiro numero: '))
+var2  = int(input('Segundo numero : '))
+
+print('Variavel 1: ',var1)
+print('Variavel 2: ',var2)
+print('Invertendo...')
+
+aux  = var2
+var2 = var1
+var1 = aux
+    
+print('Variavel 1: ',var1)
+print('Variavel 2: ',var2)
---
+++ exercicio_produto_mais_barato.py
@@ -0,0 +1,20 @@
+"""
+Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar,
+sabendo que a decisão é sempre pelo mais barato.
+"""
+
+produto_a = input('qual o preço do produto ? ')
+produto_b = input('qual o preço do outro produto ? ')
+produto_c = input('qual o preço do ultimo produto ? ')
+
+menor_preco = produto_a
+
+if menor_preco < produto_b and menor_preco < produto_c:
+    menor_preco
+elif produto_b < menor_preco and produto_b < produto_c:
+    menor_preco = produto_b
+else:
+    menor_preco = produto_c
+
+
+print(f'o produto escolhido pelo menor preço é : {menor_preco}')

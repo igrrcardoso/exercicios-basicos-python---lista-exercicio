f0987028ca4fe7dfbb4864c5188fc20bc4063009
igrrcardoso <programacaoudemyigr@gmail.com>
2023-06-09 12:57:48 -0300

    ExercicioDomHelder_2

--- exercicio_dom_helder_2.py
+++ exercicio_dom_helder_2.py
@@ -164,3 +164,93 @@ senão informar “NÚMERO TEM QUE TER 4 DÍGITOS”
 #     exit()
 
 # print(numero_inverso)
+
+""" 
+Uma operadora de TV a cabo nacional oferece uma série de serviços aos seus
+assinantes. A conta a ser paga por um assinante, no fim do mês,
+é constituída por um valor fixo mensal somado ao consumo dos canais
+pay-per-view, que são cobrados por uso diário,
+ambos de acordo com o pacote que ele contratou (ver tabela 1),
+mais os serviços extras, além da cobrança dos impostos sobre os serviços
+prestados que varia conforme a cidade do assinante (ver tabela 2).
+
+Faça um programa que solicite ao usuário o código do seu pacote,
+a quantidade de dias de consumo de canais pay-per-view,
+o valor dos serviços extras, e a cidade do assinante,
+e calcule e imprima o valor da conta que o assinante deverá pagar.
+
+Conta = Valor Fixo + Diárias Canais PPP + Serviços Extras + Imposto
+(sobre Fixo/PPP/Extras)
+"""
+# inputs
+codigo_input = input('qual o código da sua assinatura [1] [2] [3]: ')
+diaria_input = input('quantos dias de consumo em canais PPP: ')
+extras_input = input('qual o valor dos serviços extras: ')
+cidade_input = input(
+    'você é de qual cidade?'
+    '[BH] [SP] [RJ] [OU]tras cidades: '
+).upper()
+# set variáveis.
+valor_fixo = 0
+diaria = 0
+imposto = 0
+diaria_float = 0
+extras_float = 0
+# conversão str para float.
+try:
+    diaria_float = float(diaria_input)
+    extras_float = float(extras_input)
+# verificando oque foi digitado no input cidade.
+    if cidade_input not in ["BH", "SP", "RJ", "OU"]:
+        print(
+            'Valor Inválido.\nDigite a cidade novamente '
+            'usando o guia:\n'
+            'BH = Belo Horizonte\n'
+            'SP = São Paulo\n'
+            'RJ = Rio De Janeiro\n'
+            'OU = Outras Cidades\n'
+            )
+# verificando oque foi digitado no input codigo.
+    elif len(codigo_input) > 1:
+        print(
+            f'!Erro!\nO Erro está em Código: "{codigo_input}", para corrigir:'
+            '\n'
+            'Digite um código por vez.'
+            )
+    elif codigo_input not in ["1", "2", "3"]:
+        print(
+            f'!Erro!\nO Erro está em Código: "{codigo_input}", para corrigir:'
+            '\n'
+            'Digite um plano válido.\n'
+            'Planos Válidos = [1]Basic, [2]Advanced, [3]Master'
+            )
+        exit()
+
+# calculando valor da diária e setando valor fixo para cada cidade.
+    elif codigo_input == '1':
+        valor_fixo = 65
+        valor_máximo = 65
+        diaria = min(diaria_float * 1.20, valor_máximo)
+    elif codigo_input == '2':
+        valor_fixo = 104
+        diaria = diaria_float * 2.10
+    elif codigo_input == '3':
+        valor_fixo = 137
+# setando valor de imposto para cada cidade.
+    if cidade_input == 'BH':
+        imposto = 0
+    elif cidade_input == 'SP':
+        imposto = 0.01
+    elif cidade_input == 'RJ':
+        imposto = 0.015
+    else:
+        imposto = 0.2
+# calculando o valor total da conta e exibindo na tela.
+    valor_total = (valor_fixo + diaria + extras_float) * (1 + imposto)
+    print(f'Valor Total da conta = {valor_total:,.2f}')
+except ValueError:
+    print(
+        'Erro no código.\n'
+        f'O erro está em "{diaria_input}" ou em "{extras_input}" \n'
+        'Para corrigir você deve digitar apenas números nestas linhas.'
+    )
